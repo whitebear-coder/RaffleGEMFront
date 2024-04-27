e744590c8d1bd5929d7825c5b379cf3fa5749ec8
小傅哥 <184172133@qq.com>
2024-04-27 15:32:58 +0800

    feat: 前端页面 第4节：活动信息API迭代和功能完善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import {LuckyWheelPage} from "@/app/pages/lucky/lucky-wheel-page";
 import {LuckyGridPage} from "@/app/pages/lucky/lucky-grid-page";
 import dynamic from "next/dynamic";
 
-const StrategyArmoryButton = dynamic(async()=>(await import("./components/StrategyArmory")).StrategyArmory)
+const StrategyArmoryButton = dynamic(async () => (await import("./components/StrategyArmory")).StrategyArmory)
 
 export default function Home() {
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+             style={{backgroundImage: "url('/background.svg')", backgroundColor:"#e7305e;"}}>
             {/* 头部文案 */}
-            <header className="text-3xl font-bold text-center text-gray-800 my-8">
+            <header className="text-7xl font-bold text-center text-gray-800 my-8" style={{color:"white"}}>
                 大营销平台 - 抽奖展示
             </header>
 
@@ -33,8 +34,9 @@ export default function Home() {
             </div>
 
             {/* 底部文案 */}
-            <footer className="text-gray-600 text-center my-8">
-                本项目为 星球「码农会锁」第8个实战项目 <a href='https://gaga.plus' target='_blank'>https://gaga.plus</a> @小傅哥
+            <footer className="text-gray-600 text-center my-8" style={{color:"white"}}>
+                本项目为 星球「码农会锁」第8个实战项目 <a href='https://gaga.plus'
+                                                        target='_blank'>https://gaga.plus</a> @小傅哥
             </footer>
         </div>
     );
